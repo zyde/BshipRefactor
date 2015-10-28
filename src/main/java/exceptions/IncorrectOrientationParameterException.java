aa9343df76748abd887dc18fa99d36f4103d564d
Madis Bender <madis.bender@ttu.ee>
2015-10-28 22:54:34 +0200

    move in to packages, code works

---
+++ src/main/java/exceptions/IncorrectOrientationParameterException.java
@@ -0,0 +1,24 @@
+package src.main.java.exceptions;
+/*
+ * Author: Michael
+ * Created: 04 January 2005 16:54:54
+ * Modified: 04 January 2005 16:54:54
+ */
+
+
+public class IncorrectOrientationParameterException extends RuntimeException
+{
+	
+	public IncorrectOrientationParameterException()
+	{
+	}
+	
+	public IncorrectOrientationParameterException(String reason)
+	{
+		super(reason);
+	}
+	
+
+}
+
+
---
+++ src/main/java/exceptions/PositionOccupiedException.java
@@ -0,0 +1,20 @@
+package src.main.java.exceptions;
+/*
+ * Author: Michael
+ * Created: 11 November 2004 19:58:04
+ * Modified: 11 November 2004 19:58:04
+ */
+
+
+public class PositionOccupiedException extends RuntimeException
+{
+	public PositionOccupiedException()
+	{
+	}
+	
+	public PositionOccupiedException(String reason)
+	{
+		super(reason);
+	}
+
+}
