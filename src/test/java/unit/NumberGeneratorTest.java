9d7e3e90b12f9adb39aeff52a97e8ee1dbc117a6
Henri <liiv.henri@gmail.com>
2015-11-05 23:25:52 +0200

    Randomnumber test

---
+++ src/test/java/unit/NumberGeneratorTest.java
@@ -0,0 +1,30 @@
+package src.test.java.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import src.main.java.NumberGenerator;
+
+public class NumberGeneratorTest {
+    
+    private NumberGenerator numberGenerator;
+    
+    @Before 
+    public void initialize() {
+	 numberGenerator = new NumberGenerator();
+    }
+    
+	    
+
+    @Test
+    public void testRandBetween() {
+	int random = numberGenerator.rand(20);
+	int high = 20;
+	int low = 1;
+	assertTrue("Error, random is too high", high >= random);
+	assertTrue("Error, random is too low",  low  <= random);
+    }
+
+}
